4123349ec22646b1d5c0e5568e89e557f9561879
nabilaws <sekhernabil@gmail.com>
2020-06-23 18:13:41 +0200

    Well it works ..
    Display IDs , just need to push as a hierarchy parameters to SSM

--- aws-eks-tagging.ps1
+++ aws-eks-tagging.ps1
@@ -1,6 +1,6 @@
 
 #Requires -Modules @{ModuleName='AWS.Tools.Common';ModuleVersion='4.0.5.0'}
-#Requires -Module AWS.Tools.S3, AWS.Tools.EC2, AWS.Tools.Organizations, AWS.Tools.SecurityToken
+#Requires -Module AWS.Tools.S3, AWS.Tools.EC2, AWS.Tools.Organizations, AWS.Tools.SecurityToken, AWS.Tools.EKS
 
 EKS Create an ASG with an EC2 Launch template 
 EKS Publish ASG Hook to SNS 
@@ -8,3 +8,14 @@ EKS Publish ASG Hook to SNS
 
 #1:  Update the ASG and or Launch Template create by EKS (requires )
 #2:  Provide a Cloudformation logic for Cluster creation (with instant modification of the ASG and Launch template to include map tags)
+
+
+$ListMigTasks = (Get-MHMigrationTaskList -Region us-west-2)
+
+foreach ($Servers in $ListMigTasks){
+    Get-MHDiscoveredResourceList -MigrationTaskName Servers.MigrationTaskName -ProgressUpdateStream Servers.ProgressUpdateStream -Region us-west-2
+
+}
+
+
+
