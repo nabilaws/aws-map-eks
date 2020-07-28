61f0c0681f0cbff2d08d8a1644ad1404eacbc8a0
nabilaws <sekhernabil@gmail.com>
2020-07-28 17:49:18 +0200

    fixing errors

--- aws-eks-tagging/bin/Release/netcoreapp3.1/linux-x64/aws-eks-tagging.ps1
+++ aws-eks-tagging/bin/Release/netcoreapp3.1/linux-x64/aws-eks-tagging.ps1
@@ -1,23 +1,33 @@
-
 #Requires -Modules @{ModuleName='AWS.Tools.Common';ModuleVersion='4.0.6.0'}
-
 #Requires -Module AWS.Tools.EC2, AWS.Tools.AutoScaling, AWS.Tools.EKS
 #1: Tag your EKS Node during deployment
 #2: CWE trap with details to moify ASG
 #3: Customer need to refresh node group instances or wait for recycle
 #Print Event
+Write-Information -MessageData LambdaInput
 Write-Host (ConvertTo-Json -InputObject $LambdaInput -Compress -Depth 5)
-Write-Host $LambdaInput.detail
-Write-Host $LambdaInput.detail.requestParameters.tags
+Write-Information -MessageData "ConvertFrom Json"
+ConvertFrom-Json -InputObject $LambdaInput.detail
+
+$vJson = $LambdaInput.detail.requestParameters.tags.value
+Write-Information -MessageData "Print vjson tag value"
+Write-Host $vJson -Verbose
+
+
+
+$vJson2 = ConvertTo-Json -InputObject $LambdaInput -Compress -Depth 5
+$vJson2.detail.requestParameters.tags.value
+Write-Information -MessageData "vjson2 print"
+Write-Host $vJson2 -Verbose
+
 #Catch the ASG Creation from EKS Node Service
-$vJson = (ConvertTo-Json -InputObject $LambdaInput -Compress -Depth 5)
-Write-Host $vJson
+
 #$vJson = ($vJson | ConvertTo-Json )
 #Find EKS Node Name
 Write-Host $vJson.detail
-Write-Host $vJson.detail.requestParameters
-   $NodeName = ($vJson.detail.requestParameters.tags | Where-Object key -eq eks:nodegroup-name).Value
-   $ClusterName = ($vJson.detail.requestParameters.tags | Where-Object key -eq eks:cluster-name).Value
+Write-Host $vJson.detail.requestParameters.tags.value
+   $NodeName = ($vJson | Where-Object key -eq eks:nodegroup-name).Value
+   $ClusterName = ($vJson | Where-Object key -eq eks:cluster-name).Value
 Write-Host $NodeName $ClusterName   
 #Retrieve Cluster Tagging 
 Get-EKSNodegroupList -ClusterName $ClusterName
