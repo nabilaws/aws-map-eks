708fb296ccb0c78d888a52dc5c57282b1b4d32df
nabilaws <sekhernabil@gmail.com>
2020-07-29 19:38:01 +0200

    done

--- aws-eks-tagging.ps1
+++ aws-eks-tagging.ps1
@@ -4,34 +4,29 @@
 #2: CWE trap with details to moify ASG
 #3: Customer need to refresh node group instances or wait for recycle
 #Print Event
-Write-Host $LambdaContext
-Write-Host $LambdaInput.detail
-$vJson2 = (ConvertFrom-Json -InputObject $LambdaInput -Verbose)
-$jsonTags = ($vJson2.detail.requestParameters.tags)
-$eksASG = ($vJson2.detail.requestParameters.autoScalingGroupName)
-Write-Information -MessageData "vjson2 print"
-Write-Host $jsonTags.Value
-#Catch the ASG Creation from EKS Node Service
-#$vJson = ($vJson | ConvertTo-Json )
-#Find EKS Node Name
+Write-Host 'Log group name:' $LambdaContext.LogGroupName
 try {
-   $ClusterName = ($jsonTags | Where-Object key -eq "eks:cluster-name" ).Value
-   $NodeName = ($jsonTags | Where-Object key -eq "eks:nodegroup-name" ).Value
+   $ClusterName = ($LambdaInput.detail.requestParameters.tags | Where-Object key -eq "eks:cluster-name").Value 
+   $NodeName = ($LambdaInput.detail.requestParameters.tags | Where-Object key -eq "eks:nodegroup-name").Value
 }
 catch {
    Write-Host $($_.exception.message)
-   Break
 }
-Write-Information -Message "Node and Cluster names from tags"
-Write-Host $NodeName $ClusterName   
+if($NodeName -eq $ ""){
+   Write-Error "Dong! Json not deserialized"
+}
+else {
+   Write-Information -Message "Node and Cluster names from tags"
+   Write-Host $NodeName $ClusterName         
+}
 #Retrieve Cluster Tagging 
 $MAPTagValue = (Get-EKSNodegroup -ClusterName $ClusterName -NodegroupName $NodeName).Tags.Values
 #Prepare AS Tags
 $EKSTags = (New-Object Amazon.AutoScaling.Model.Tag)
 $EKSTags.ResourceType = "auto-scaling-group"
-$EKSTags.ResourceId = $eksASG
+$EKSTags.ResourceId = $LambdaInput.detail.requestParameters.autoScalingGroupName
 $EKSTags.Key = "map-migrated"
 $EKSTags.Value = $MAPTagValue
 $EKSTags.PropagateAtLaunch = $true
-#Aplpy Tags to ASG for future launch
-Set-ASTag -Tag $EKSTags 
+#Aplpy Tags to ASG for future launch 
+$ModASG = Set-ASTag -Tag $EKSTags -Verbose
