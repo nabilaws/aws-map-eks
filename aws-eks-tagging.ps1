e2c1a608a3b887c18b2c04407a95af6791a5ed66
nabilaws <sekhernabil@gmail.com>
2020-10-07 19:06:23 +0200

    fix enumarator

--- aws-eks-tagging.ps1
+++ aws-eks-tagging.ps1
@@ -2,7 +2,7 @@
 #Requires -Module AWS.Tools.EC2, AWS.Tools.AutoScaling, AWS.Tools.EKS
 #1: Tag your EKS Cluster with "map-migrated" and associated value to retrieve it on your EC2 resources
 
-$TagsList = ($LambdaInput.detail.requestParameters.resourcesSet)
+$TagsList = ($LambdaInput.detail.requestParameters.tags)
 
 try {
    $ClusterName = ($LambdaInput.detail.requestParameters.tags | Where-Object key -eq "eks:cluster-name").Value 
@@ -19,14 +19,15 @@ else {
    Write-Host $NodeName $ClusterName         
 }
 #Retrieve node tagging 
-$MAPTagValue = (Get-EKSNodegroup -ClusterName $ClusterName -NodegroupName $NodeName).Tags.Values | Where-Object key -eq "map-migrated"
+$MAPTagValueList = (Get-EKSNodegroup -ClusterName $ClusterName -NodegroupName $NodeName).Tags
+$MAPTagValue = $MAPTagValueList.GetEnumerator() | Where-Object Key -eq "map-migrated"
 
 #Prepare AS Tags
 $EKSTags = (New-Object Amazon.AutoScaling.Model.Tag)
 $EKSTags.ResourceType = "auto-scaling-group"
 $EKSTags.ResourceId = $LambdaInput.detail.requestParameters.autoScalingGroupName
 $EKSTags.Key = "map-migrated"
-$EKSTags.Value = $MAPTagValue
+$EKSTags.Value = $MAPTagValue.Value
 $EKSTags.PropagateAtLaunch = $true
 #Apply tags to ASG for future launch
 Write-Host "Tempo for 5 seconds"
@@ -51,7 +52,7 @@ Get-EC2Instance
 $EC2inASG = $eksASG.Instances.InstanceId
 $EC2Tag = New-Object Amazon.EC2.Model.Tag
 $EC2Tag.Key = "map-migrated"
-$EC2Tag.Value = $MAPTagValue
+$EC2Tag.Value = $EKSTags.Value
 Foreach ($EC2List in $EC2inASG) 
 {
    Write-Host "Tagging" $EC2List
